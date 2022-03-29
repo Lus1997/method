af16057c33b9ab3ad080edf1bf0aae752d5dc738
Lusine1920 <lusine.arshakyan.97@mail.ru>
2022-03-30 00:34:15 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public static int fib(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fib(n-2) + fib(n-1);
+    }
+    public static void main(String args[]) {
+        int maxNumber = 10;
+        System.out.print("Fibonacci Series of "+maxNumber+" numbers: ");
+        for(int i = 0; i < maxNumber; i++){
+            System.out.print(fib(i) +" ");
+        }
+    }
+}
